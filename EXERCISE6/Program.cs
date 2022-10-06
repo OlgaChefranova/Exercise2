519d6ce94640ba5faf4372b320a8677e28a0a568
OLGA <ochefranova@yandex.ru>
2022-10-07 00:46:12 +0500

    задача 6

---
+++ EXERCISE6/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число и выдает, является ли число
+//четным.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number  = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} чётное число ");
+}
+else
+{
+    Console.WriteLine($"{number} не является чётным числом ");
+}
